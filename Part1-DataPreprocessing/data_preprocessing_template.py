eea73318e1d8054a114b4d48b0e6ff277cae706b
zara-sh <zara.regist@gmail.com>
2018-04-15 19:31:59 +0900

    import lib & add dataset

---
+++ Part1-DataPreprocessing/data_preprocessing_template.py
@@ -0,0 +1,9 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset= pd.read_csv('Data.csv')
+# Importing the dataset
