b649250421f941af1a49bedfc65f3e70f0a4833f
zara-sh <zara.regist@gmail.com>
2018-04-30 11:11:48 +0900

    create linear regression

--- Part2-Regression/Section4-SimpleLinearRegression/Simple_Linear_Regression/my_simple_linear_regression.py
+++ Part2-Regression/Section4-SimpleLinearRegression/Simple_Linear_Regression/my_simple_linear_regression.py
@@ -18,3 +18,9 @@ list(dataset)
 # split dataset into Test and Training set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fitting Simple Linear Regression to the Training set
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
