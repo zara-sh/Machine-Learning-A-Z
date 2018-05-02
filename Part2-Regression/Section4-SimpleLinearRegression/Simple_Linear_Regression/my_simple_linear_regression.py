69da741d7f9540da384274c39fd17d586c9ca73f
zara-sh <zara.regist@gmail.com>
2018-05-02 17:51:53 +0900

    linear regression to predict the salary

--- Part2-Regression/Section4-SimpleLinearRegression/Simple_Linear_Regression/my_simple_linear_regression.py
+++ Part2-Regression/Section4-SimpleLinearRegression/Simple_Linear_Regression/my_simple_linear_regression.py
@@ -24,3 +24,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, rando
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+ 
+
+# Predict the Test set results
+y_predict = regressor.predict(X_test)
