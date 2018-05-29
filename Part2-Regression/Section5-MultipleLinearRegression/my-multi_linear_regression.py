76715b7d54a6dec6f0899915b80db0097904a7d5
zara-sh <zara.regist@gmail.com>
2018-05-29 23:31:59 +0900

    prediction with all independent var

--- Part2-Regression/Section5-MultipleLinearRegression/my-multi_linear_regression.py
+++ Part2-Regression/Section5-MultipleLinearRegression/my-multi_linear_regression.py
@@ -24,4 +24,14 @@ X = X[:, 1:]
 
 #split the data into the training set and test set
 from sklearn.cross_validation import train_test_split
-X_trian, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# Fitting Multiple Linear Regresion to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# predition the  Test set result
+
+# Building Optimal model using Backward Elimination
+import statsmodels.formula.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int), values = X, axis = 1)
